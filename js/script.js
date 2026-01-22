363d95aec7ca370257f13054b9ebcb6f1be8f908
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-01-22 15:11:19 +0200

    Add script.js for date and time functionality

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const d = new Date();
+console.log(d);
+
+let day = d.getDay();
+console.log(day);
+
+let date = d.getDate();
+console.log(date);
+
+let month = d.getMonth();
+console.log(month);
+
+let year = d.getFullYear();
+console.log(year);
